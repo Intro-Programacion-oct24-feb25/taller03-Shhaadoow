a75ad8f0802e5d68e18db2fd7963f761fe59385c
Pablo Ordóñez <pablosebas61@gmail.com>
2024-10-24 11:52:55 -0500

    Taller grupal

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -16,7 +16,10 @@ public class Ejercicio03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        int var1 = 10;
+        int var2 = 2;
+        int var3 = 16;
+        System.out.println(Math.pow(var1, var2) + Math.sqrt(var3) + 20 / (1 + 11));
     }
-    
+
 }
---
+++ Ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio05;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        System.out.println(Math.pow(2, 2)+12-1 / (3));
+    }
+    
+}
